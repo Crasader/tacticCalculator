d37394f0a4d10b1f19512898b9bd16c4a864bd84
David Acs <david.acs@cheppers.com>
2018-07-10 15:12:59 +0200

    Add to 1000 case

--- js/tacticsScript.js
+++ js/tacticsScript.js
@@ -3,14 +3,14 @@ $("#calculateButton").click(function() {
     round=0;
     console.log(actualMoney);
 
-    maxMoney = actualMoney;
+    var maxMoney = [];
 
     //bets = [60.24, 132.82, 292.84, 645.86, 1423.57, 3138.71, 6300, 13000];
-    bets = [50, 110, 242, 472, 911, 1725, 3240, 6040, 13000, 26000];
-    //bets = [100, 220, 484, 944, 1822, 3600, 7400];
-    //bets = [200, 440, 940, 1800, 3600];
+    bets = [50, 110, 242, 472, 911, 1717, 3216, 5991, 11142];
+    //bets = [100, 220, 484, 944, 1822, 3424, 6404, 11911];
+    //bets = [200, 440, 940, 1800, 3600, ];
     //bets = [50, 100, 350, 750, 1500, 3000, 6000, 12000];
-    odd = 1.9;
+    odd = 1.83;
 
     sumCase = 0;
     lostNumber = 0;
@@ -19,9 +19,23 @@ $("#calculateButton").click(function() {
     win = 0;
     lost = 0;
 
-    while (actualMoney < 20000 && sumCase < 500) {
+    winRound = 0;
+    lostRound = 0;
+    finishMoney = 40000;
+
+    while (sumCase < 10000) {
+        console.log(sumCase + ": " +actualMoney);
         sumCase++;
-        actualMoney=5000;
+        if (sumCase > 1) {   
+            maxMoney[maxMoney.length] = actualMoney; 
+            //maxMoney.push(actualMoney+' ');
+            if(actualMoney >= finishMoney) {
+                winRound++; 
+            } else {
+                lostRound++;
+            }
+        }
+        actualMoney = $("#startMoney").html() * 1;;
 
         if(maxRound < round) {
             maxRound = round;
@@ -30,7 +44,8 @@ $("#calculateButton").click(function() {
             minRound = round;
         }
         round = 0;
-        while (actualMoney > 0 && actualMoney < 20000) {
+        lostNumber=0;
+        while (actualMoney > 0 && actualMoney < finishMoney) {
 
             //if(lostNumber == 0) {
                 myTip = (Math.floor((Math.random() * 100)));
@@ -55,16 +70,16 @@ $("#calculateButton").click(function() {
             //    lostNumber = 0;
            // }
 
-            if (actualMoney > maxMoney) {
-                maxMoney = actualMoney;
-            }
+           // if (actualMoney > maxMoney) {
+           //     maxMoney = actualMoney;
+           // }
 
             //console.log(actualMoney + " " + lostNumber + " g:" + grow);
             round++;
             $("div#round").html(round);
 
             $("div#actualMoney").html(actualMoney);
-            $("div#maxMoney").html(maxMoney);
+            //$("div#maxMoney").html(maxMoney);
             $("div#lost").html(lost);
             $("div#win").html(win);
         }
@@ -75,6 +90,11 @@ $("#calculateButton").click(function() {
 
         $("div#sumRound").html(lost+win);
     }    
+
+
+    console.log(maxMoney);
+    $("div#lostRound").html(lostRound);
+    $("div#winRound").html(winRound);
   });
 
   //esetek: 
