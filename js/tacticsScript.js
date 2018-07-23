06e6441363675327ad449d899ee41f4aaccbca97
David Acs <david.acs@cheppers.com>
2018-07-23 17:33:18 +0200

    hányados bevezetése

--- js/tacticsScript.js
+++ js/tacticsScript.js
@@ -1,18 +1,27 @@
 $("#calculateButton").click(function() {
     actualMoney = $("#startMoney").html() * 1;
+    startMoney = $("#startMoney").html() * 1;
     round=0;
     console.log(actualMoney);
 
     var maxMoney = [];
 
+    //hanyados = [100, 45.45, 20.66, 9.39, 4.27, 1.94, 0.88];
+    //
+    hanyados = [50, 22.7, 10.33, 4.7, 2.13, 1, 0.44];
+    //hanyados = [33.33, 15.15, 6.89, 3.13, 1.42, 0.65, 0.29];
     //bets = [60.24, 132.82, 292.84, 645.86, 1423.57, 3138.71, 6300, 13000];
+
     //bets = [50, 110, 242, 472, 911, 1717, 3216, 5991, 11142];
+
     //bets = [100, 220, 484, 944, 1822, 3424, 6404, 11911];
     //bets = [200, 440, 940, 1800, 3600, 7800];
-    bets = [400, 880, 1880, 3660, 6320, 12000];
     //bets = [50, 100, 350, 750, 1500, 3000, 6000, 12000];
     odd = 1.83;
 
+    bets = getBets(startMoney, hanyados);
+
+    console.log(bets);
     sumCase = 0;
     lostNumber = 0;
     maxRound = 0;
@@ -22,10 +31,11 @@ $("#calculateButton").click(function() {
 
     winRound = 0;
     lostRound = 0;
-    finishMoney = actualMoney*4;
+    finishMoney = actualMoney*3;
 
-    while (sumCase < 10000) {
+    while (sumCase < 1000) {
         console.log(sumCase + ": " +actualMoney);
+        console.log(bets);
         sumCase++;
         if (sumCase > 1) {   
             maxMoney[maxMoney.length] = actualMoney; 
@@ -49,8 +59,9 @@ $("#calculateButton").click(function() {
         while (actualMoney > 0 && actualMoney < finishMoney) {
 
             //if(lostNumber == 0) {
-                myTip = (Math.floor((Math.random() * 100)));
-            //}
+            //    myTip = (Math.floor((Math.random() * 100)));
+           // }
+            myTip=0;
             result = (Math.floor((Math.random() * 100)));
 
             if (myTip%2 == result%2) {
@@ -58,6 +69,7 @@ $("#calculateButton").click(function() {
                 actualMoney = Math.round(actualMoney + grow);
                 lostNumber=0;
                 win++;
+                bets = getBets(actualMoney, hanyados);
             } else {
                 actualMoney = Math.round(actualMoney - bets[lostNumber]);
                 grow = - bets[lostNumber];
@@ -74,7 +86,7 @@ $("#calculateButton").click(function() {
            // if (actualMoney > maxMoney) {
            //     maxMoney = actualMoney;
            // }
-
+//break;
             //console.log(actualMoney + " " + lostNumber + " g:" + grow);
             round++;
             $("div#round").html(round);
@@ -96,8 +108,19 @@ $("#calculateButton").click(function() {
     console.log(maxMoney);
     $("div#lostRound").html(lostRound);
     $("div#winRound").html(winRound);
+    
   });
 
+function getBets(money, hanyados) {
+    return [
+        Math.round(money/hanyados[0]), 
+        Math.round(money/hanyados[1]), 
+        Math.round(money/hanyados[2]), 
+        Math.round(money/hanyados[3]), 
+        Math.round(money/hanyados[4]), 
+        Math.round(money/hanyados[6])
+    ];
+}
   //esetek: 
   // - kis tét duplázás végig nyeremény,
   // - kis tét duplázás nincs nyeremény csak 1. 2.,
