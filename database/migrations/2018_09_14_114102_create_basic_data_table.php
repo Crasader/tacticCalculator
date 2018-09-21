fd19fbc3058f65f560fd576eb09bb9806d50aebc
David Acs <david.acs@cheppers.com>
2018-09-21 15:04:34 +0200

    First Migration

---
+++ database/migrations/2018_09_14_114102_create_basic_data_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBasicDataTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('basic_data', function (Blueprint $table) {
+            $table->string('key')->unique();
+            $table->string('value')->nullable();
+            $table->timestamp('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('basic_data');
+    }
+}
